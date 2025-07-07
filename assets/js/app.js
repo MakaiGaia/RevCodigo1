806f2591995a2ad23bacbf225fd53c30335a1d71
Alfonso Esau <kernelsoundstudio@gmail.com>
2025-07-07 17:18:12 -0600

    comments on what i've just fix'd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,24 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
+//Corrección del indicador en name, para usarlo con ID
 const $n = document.querySelector('#name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
-
+//Cambio de función a función asincrona
 async function displayUser(username) {
   $n.textContent = 'cargando...';
+  //Try catch agregado para la función await
   try {    
     const response = await fetch(`${usersEndpoint}/${username}`);
+    //Creación de la variable data para su uso posterior
     const data = await response.json();
     console.log(data);
+    //Corrección de comilla simple a backticks
     $n.textContent = `${data.name}`;
     $b.textContent = `${data.blog}`;
     $l.textContent = `${data.location}`;
   } catch (err) {
+    //Uso de la funcion handleError
     handleError(err);
   }
     
